ab1d3731f180ca4127ed50bdff2306cbc32e0d57
Alex-McAvoy <931772462@qq.com>
2026-02-06 23:15:06 +0800

    [Feat] 新增全局配置

---
+++ config.js
@@ -0,0 +1,11 @@
+// 应用全局配置
+module.exports = {
+  transpileDependencies: ["uview-ui"],
+  baseUrl: "https://github.com/Alex-McAvoy/Whatever",
+  appInfo: {
+    // 应用名称
+    name: "Whatever",
+    // 应用版本
+    version: "v0.1.0-alpha",
+  },
+}
