abb53880af12cd1d296d9632e062ca96375223af
Alex-McAvoy <931772462@qq.com>
2026-02-06 23:20:10 +0800

    [Feat] 新增Navbar组件

---
+++ components/mineNavbar/mineNavbar.vue
@@ -0,0 +1,50 @@
+<!--
+ * @Description   上方navbar组件
+ * @Author        Alex_McAvoy
+ * @Date          2026-02-06 22:39:56
+-->
+<template>
+	<view>
+		<u-navbar :title="title" height="44" :autoBack="true" :placeholder="true" :bgColor="navbarVar.bgColor"
+			:titleStyle="navbarVar.titleStyle">
+			<view class="u-nav-slot" slot="left">
+				<u-icon name="/static/images/common/narbar/arrow.png" size="20"></u-icon>
+			</view>
+		</u-navbar>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "mineNavbar",
+		props: {
+			// title
+			title: {
+				type: String,
+				default: ""
+			}
+		},
+		data() {
+			return {
+				// 导航栏
+				navbarVar: {
+					// 背景色
+					bgColor: "#1e1b1b",
+					// 文字样式
+					titleStyle: {
+						"font-family": "heiTi",
+						"font-size": "20px",
+						"font-weight": "bold",
+						"-webkit-background-clip": "text",
+						"-webkit-text-fill-color": "transparent",
+						"textShadow": "0px 0px 7px rgba(52, 255, 204, 0.1)",
+						"background-image": "-webkit-linear-gradient(top, #aca08d,#f3f1f4,#b9b196)",
+					}
+				},
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
