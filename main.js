0b6afcc2a94d9f621dc1d517017b21731b61ce68
Alex-McAvoy <931772462@qq.com>
2026-02-08 01:00:47 +0800

    [Feat] 新增main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+import App from './App'
+import uView from "uview-ui";
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+
+import pageSearch from '@/components/pageSearch/pageSearch.vue'
+import mineNavbar from '@/components/mineNavbar/mineNavbar.vue'
+import mineTabbar from '@/components/mineTabbar/mineTabbar.vue'
+
+Vue.use(uView);
+Vue.component('page-search', pageSearch);
+Vue.component('mine-navbar', mineNavbar);
+Vue.component('mine-tabbar', mineTabbar);
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+	...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import {
+	createSSRApp
+} from 'vue'
+export function createApp() {
+	const app = createSSRApp(App)
+	return {
+		app
+	}
+}
+// #endif
